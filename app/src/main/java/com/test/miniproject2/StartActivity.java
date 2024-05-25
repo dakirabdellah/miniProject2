5a09d7872f44812eb073c2e71856a11131cbf7a8
Dakir <dakirabdellah3@gmail.com>
2024-05-26 00:29:19 +0100

    Get Random Quote using JsonObjectRequest

--- app/src/main/java/com/test/miniproject2/StartActivity.java
+++ app/src/main/java/com/test/miniproject2/StartActivity.java
@@ -2,7 +2,6 @@ package com.test.miniproject2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
@@ -11,6 +10,7 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
@@ -21,7 +21,6 @@ public class StartActivity extends AppCompatActivity {
     TextView tvStartActQuote, tvStartActAuthor;
     Button btnStartActPass;
 
-    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,17 +35,14 @@ public class StartActivity extends AppCompatActivity {
         String url = "https://dummyjson.com/quotes/random";
 
         // Request a string response from the provided URL.
-        StringRequest stringRequest = new StringRequest(
-                Request.Method.GET, url,
-                new Response.Listener<String>() {
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                url,
+                new Response.Listener<JSONObject>() {
                     @Override
-                    public void onResponse(String response) {
-                        // Display the first 500 characters of the response string.
+                    public void onResponse(JSONObject response) {
                         try {
-                            JSONObject jsonObject = new JSONObject(response);
-
-                            tvStartActQuote.setText(jsonObject.getString("quote"));
-                            tvStartActAuthor.setText(jsonObject.getString("author"));
+                            tvStartActQuote.setText(response.getString("quote"));
+                            tvStartActAuthor.setText(response.getString("author"));
                         } catch (JSONException e) {
                             throw new RuntimeException(e);
                         }
@@ -55,13 +51,11 @@ public class StartActivity extends AppCompatActivity {
                 new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
-                        error.printStackTrace();
-                        tvStartActQuote.setText("That didn't work!");
+
                     }
                 });
 
-        stringRequest.setTag("TAG");
-        queue.add(stringRequest);
+        queue.add(jsonObjectRequest);
 
         //endregion
 
