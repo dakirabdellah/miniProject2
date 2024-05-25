82816ba3244803269389087ec545d675008f38c4
Dakir <dakirabdellah3@gmail.com>
2024-05-25 20:44:35 +0100

    Initial commit

---
+++ app/src/main/java/com/test/miniproject2/StartActivity.java
@@ -0,0 +1,72 @@
+package com.test.miniproject2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class StartActivity extends AppCompatActivity {
+    TextView tvStartActQuote, tvStartActAuthor;
+    Button btnStartActPass;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+        tvStartActQuote = findViewById(R.id.tvStartActQuote);
+        tvStartActAuthor = findViewById(R.id.tvStartActAuthor);
+        btnStartActPass = findViewById(R.id.btnStartActPass);
+
+        //region Test Volley
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "https://dummyjson.com/quotes/rando";
+
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(
+                Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+
+                            tvStartActQuote.setText(jsonObject.getString("quote"));
+                            tvStartActAuthor.setText(jsonObject.getString("author"));
+                        } catch (JSONException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        error.printStackTrace();
+                        tvStartActQuote.setText("That didn't work!");
+                    }
+                });
+
+        stringRequest.setTag("TAG");
+        queue.add(stringRequest);
+
+        //endregion
+
+        btnStartActPass.setOnClickListener(v -> {
+            finish();
+        });
+    }
+}
