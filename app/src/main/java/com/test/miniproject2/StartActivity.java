571b123b36e40cff2990afc1012afa4794eb0e1b
Dakir <dakirabdellah3@gmail.com>
2024-05-28 21:20:24 +0100

    Pin | Unpin quote using SharedPreferences

--- app/src/main/java/com/test/miniproject2/StartActivity.java
+++ app/src/main/java/com/test/miniproject2/StartActivity.java
@@ -20,10 +20,34 @@ import org.json.JSONObject;
 
 import java.util.Random;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class StartActivity extends AppCompatActivity {
     TextView tvStartActQuote, tvStartActAuthor;
     Button btnStartActPass;
+    ToggleButton tbStartActPinUnpin;
+    SharedPreferences sharedPreferences;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,15 +56,53 @@ public class StartActivity extends AppCompatActivity {
         tvStartActQuote = findViewById(R.id.tvStartActQuote);
         tvStartActAuthor = findViewById(R.id.tvStartActAuthor);
         btnStartActPass = findViewById(R.id.btnStartActPass);
+        tbStartActPinUnpin = findViewById(R.id.tbStartActPinUnpin);
 
-        //region Test Volley
+        sharedPreferences = getSharedPreferences("pinned-quote", MODE_PRIVATE);
+
+        String quote = sharedPreferences.getString("quote", null);
+
+        if (quote == null) {
+            getRandomQuote();
+        } else {
+            String author = sharedPreferences.getString("author", null);
+
+            tvStartActQuote.setText(quote);
+            tvStartActAuthor.setText(author);
+
+            tbStartActPinUnpin.setChecked(true);
+        }
+
+        tbStartActPinUnpin.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                String quote = null;
+                String author = null;
+
+                if (isChecked) {
+                    quote = tvStartActQuote.getText().toString();
+                    author = tvStartActAuthor.getText().toString();
+                } else {
+                    getRandomQuote();
+                }
+
+                editor.putString("quote", quote);
+                editor.putString("author", author);
+
+                editor.commit();
+            }
+        });
+
+        btnStartActPass.setOnClickListener(v -> {
+            finish();
+        });
+    }
+
+    private void getRandomQuote() {
         RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "https://dummyjson.com/quotes/random";
 
-
-
-        String url ="https://dummyjson.com/quotes/random";
-
-        // Request a string response from the provided URL.
         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
                 url,
                 new Response.Listener<JSONObject>() {
@@ -62,11 +124,5 @@ public class StartActivity extends AppCompatActivity {
                 });
 
         queue.add(jsonObjectRequest);
-
-        //endregion
-
-        btnStartActPass.setOnClickListener(v -> {
-            finish();
-        });
     }
 }
